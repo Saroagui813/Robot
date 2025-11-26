6cfbc870940bca81fed0416b340e5220dc58e130
Saroagui813 <saroagui8132006@gmail.com>
2025-11-26 14:13:51 +0100

    He terminado excepcion

--- src/main/java/org/iesalandalus/programacion/robot/modelo/RobotException.java
+++
@@ -1,7 +0,0 @@
-package org.iesalandalus.programacion.robot.modelo;
-
-public class RobotException extends RuntimeException {
-    public RobotException(String message) {
-        super(message);
-    }
-}
