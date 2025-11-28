12d35f596584c9e2a45d0c5906de8cb103386db7
Saroagui813 <saroagui8132006@gmail.com>
2025-11-28 13:01:21 +0100

    Comprobado que el robot copia no sea nula.

--- src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -1,11 +1,13 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import java.util.Objects;
+
 public class ControladorRobot {
 
     private Robot robot;
 
     public ControladorRobot(Robot robot) {
-        this.robot = new Robot(robot);
+        this.robot = new Robot(Objects.requireNonNull(robot, "El robot no puede ser nulo."));
     }
 
     public Robot getRobot() {
